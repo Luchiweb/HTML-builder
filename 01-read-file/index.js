5321863ee54a76ec1ce51522f603c2755f64460e
Svetlana <svetlana.moiseienko.web@gmail.com>
2023-05-03 15:21:19 +0200

    feat: complete the task 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
+
+const filePath = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(filePath);
+
+readStream.on('data', (text) => stdout.write(text));
