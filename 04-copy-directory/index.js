7836dd5300730b7ba83d1a351b513445b1271b92
Svetlana <svetlana.moiseienko.web@gmail.com>
2023-05-04 09:32:17 +0200

    feat: complete the task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const sourcePath = path.join(__dirname, 'files');
+const targetPath = path.join(__dirname, 'files-copy');
+
+async function copyDir(source, target) {
+  try {
+    await fs.mkdir(target, { recursive: true });
+
+    const files = await fs.readdir(source);
+
+    for (const file of files) {
+      const filePath = path.join(source, file);
+      const targetFilePath = path.join(target, file);
+      const fileStat = await fs.stat(filePath);
+
+      if (fileStat.isDirectory()) {
+        await copyDir(filePath, targetFilePath);
+      } else {
+        await fs.copyFile(filePath, targetFilePath);
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+copyDir(sourcePath, targetPath);
 
