8f99dda263fb13aabaa5182f2b1a1094f901244b
Svetlana <svetlana.moiseienko.web@gmail.com>
2023-05-08 11:24:16 +0200

    refactor: change fs to fsp

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,26 +1,24 @@
-const fs = require('fs').promises;
+const fsp = require('fs').promises;
 const path = require('path');
 
 const stylesPath = path.join(__dirname, 'styles');
 const distPath = path.join(__dirname, 'project-dist');
 const bundlePath = path.join(distPath, 'bundle.css');
 
-async function mergeStyles(stylesPath, bundlePath, distPath) {
-  await fs.mkdir(distPath, { recursive: true });
-
-  const files = await fs.readdir(stylesPath);
+async function mergeStyles(stylesPath, bundlePath) {
+  const files = await fsp.readdir(stylesPath);
   const cssFiles = files.filter((file) => path.extname(file) === '.css');
 
   const styles = [];
 
   for (const file of cssFiles) {
     const filePath = path.join(stylesPath, file);
-    const css = await fs.readFile(filePath, 'utf-8');
+    const css = await fsp.readFile(filePath, 'utf-8');
 
     styles.push(css);
   }
 
-  await fs.writeFile(bundlePath, styles.join('\n'));
+  await fsp.writeFile(bundlePath, styles.join('\n'));
 }
 
-mergeStyles(stylesPath, bundlePath, distPath);
+mergeStyles(stylesPath, bundlePath);
