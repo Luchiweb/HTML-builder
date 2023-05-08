082fdb0b5c1c62df6634c1615a64c40c9c702c3c
Svetlana <svetlana.moiseienko.web@gmail.com>
2023-05-08 11:08:14 +0200

    fix: fix problem with deleting files in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,26 +1,38 @@
-const fs = require('fs').promises;
+const fsp = require('fs').promises;
 const path = require('path');
 
 const sourcePath = path.join(__dirname, 'files');
 const targetPath = path.join(__dirname, 'files-copy');
 
 async function copyDir(source, target) {
-  await fs.mkdir(target, { recursive: true });
+  const isDirExist = await isDirectoryExist();
+  if (isDirExist) await fsp.rm(target, { recursive: true });
 
-  const files = await fs.readdir(source);
+  await fsp.mkdir(target, { recursive: true });
+
+  const files = await fsp.readdir(source);
 
   for (const file of files) {
     const filePath = path.join(source, file);
     const targetFilePath = path.join(target, file);
-    const fileStat = await fs.stat(filePath);
+    const fileStat = await fsp.stat(filePath);
 
     if (fileStat.isDirectory()) {
       await copyDir(filePath, targetFilePath);
     } else {
-      await fs.copyFile(filePath, targetFilePath);
+      await fsp.copyFile(filePath, targetFilePath);
     }
   }
 
+}
+
+async function isDirectoryExist(target) {
+  try {
+    await fsp.access(target);
+    return true;
+  } catch (err) {
+    return false;
+  }
 }
 
 copyDir(sourcePath, targetPath);
